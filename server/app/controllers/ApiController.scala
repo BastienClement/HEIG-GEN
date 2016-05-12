570dc29c0bf63500669e57b990ee1b8d3920142a
galedric <g@ledric.me>
2016-05-12 10:20:25 +0200

    Add register action

--- server/app/controllers/ApiController.scala
+++ server/app/controllers/ApiController.scala
@@ -1,7 +1,8 @@
 package controllers
 
 import com.google.inject.{Inject, Singleton}
-import models.Users
+import models._
+import models.mysql._
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import scala.concurrent.ExecutionContext
@@ -20,5 +21,18 @@ class ApiController @Inject()(implicit ec: ExecutionContext) extends Controller 
 		} yield Json.obj("res" -> "ok")
 
 		res.map(Ok(_)).recover { case e => Unauthorized(Json.obj("res" -> "nok", "err" -> e.getMessage)) }
+	}
+
+	def register = Action.async(parse.json) { req =>
+		val username = (req.body \ "user").as[String]
+		val password = (req.body \ "password").as[String]
+
+		val insert = Users += User(0, username.toLowerCase, PasswordHasher.hash(password), false)
+
+		insert.run.map {
+			case _ => Ok(Json.obj("res" -> "ok"))
+		}.recover {
+			case _ => Conflict(Json.obj("res" -> "nok", "err" -> "This username is already taken"))
+		}
 	}
 }
