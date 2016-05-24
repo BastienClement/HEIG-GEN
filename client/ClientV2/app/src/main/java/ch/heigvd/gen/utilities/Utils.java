cfa308f41bb671c6423e004241232b28ee42adc4
antoine drabble <antoine.drabble@gmail.com>
2016-05-24 19:21:57 +0200

    Fix setToken and getToker

--- client/ClientV2/app/src/main/java/ch/heigvd/gen/utilities/Utils.java
+++ client/ClientV2/app/src/main/java/ch/heigvd/gen/utilities/Utils.java
@@ -10,24 +10,24 @@ import org.json.JSONObject;
 public final class Utils {
 
     public static String getToken(final Activity a) {
-        SharedPreferences prefs = a.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences prefs = a.getSharedPreferences("Authentication", Context.MODE_PRIVATE);
         return prefs.getString("Token", "");
     }
 
     public static void setToken(final Activity a, final String token) {
-        SharedPreferences prefs = a.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences prefs = a.getSharedPreferences("Authentication", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("Token", token);
         editor.commit();
     }
 
     public static String getUser(final Activity a) {
-        SharedPreferences prefs = a.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences prefs = a.getSharedPreferences("Authentication", Context.MODE_PRIVATE);
         return prefs.getString("User", "");
     }
 
     public static void setUser(final Activity a, final String user) {
-        SharedPreferences prefs = a.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences prefs = a.getSharedPreferences("Authentication", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("User", user);
         editor.commit();
