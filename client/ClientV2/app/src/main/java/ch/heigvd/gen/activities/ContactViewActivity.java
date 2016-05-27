724383bbecfdafa27b922df5e81b8ea09fbfb558
Guillaume Serneels <guillaume.serneels@heig-vd.ch>
2016-05-27 13:16:47 +0200

    had to comment line 95 : finish() to make it stable

--- client/ClientV2/app/src/main/java/ch/heigvd/gen/activities/ContactViewActivity.java
+++ client/ClientV2/app/src/main/java/ch/heigvd/gen/activities/ContactViewActivity.java
@@ -1,16 +1,24 @@
 package ch.heigvd.gen.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import ch.heigvd.gen.R;
 import ch.heigvd.gen.adapters.ChatAdapter;
+import ch.heigvd.gen.communications.RequestGET;
+import ch.heigvd.gen.interfaces.ICallback;
 import ch.heigvd.gen.interfaces.IRequests;
 import ch.heigvd.gen.models.Message;
+import ch.heigvd.gen.utilities.Utils;
 
 
 public class ContactViewActivity extends AppCompatActivity implements IRequests {
@@ -18,15 +26,32 @@ public class ContactViewActivity extends AppCompatActivity implements IRequests 
     private List<Message> list;
     ChatAdapter adapter;
 
-    private final static String TAG = ContactListActivity.class.getSimpleName();
+    Bundle b = null;
+
+    private final static String TAG = ContactViewActivity.class.getSimpleName();
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_contact_view);
 
+            // get contact
+            b = getIntent().getExtras();
+            String contact = null;
+            int id;
+            if(b != null) {
+                contact = b.getString("contact");
+                id = b.getInt("id");
+            }
+
+            // enable back button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
             //Initiate List
             list = new LinkedList<>();
+
+            //Pour tester avant que les messages soient implémentés coté serveur
+
 
             // Create the ChatAdapter
             adapter = new ChatAdapter(this, R.layout.other_message_list_item, list);
@@ -37,5 +62,49 @@ public class ContactViewActivity extends AppCompatActivity implements IRequests 
             listView.setAdapter(adapter);
 
 
+            // set contact name
+            TextView title = (TextView) findViewById(R.id.contact_name);
+            title.setText(contact);
+
+
+
+
         }
+
+    public void editContact(final View view){
+        // start contact search activity
+        Intent intent = new Intent(ContactViewActivity.this, ContactEditActivity.class);
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onResume()
+    {  // After a pause OR at startup
+        System.out.println(b.getInt("id"));
+        super.onResume();
+        // Check if it is still a contact
+        new RequestGET(new ICallback<String>() {
+            @Override
+            public void success(String result) {
+                Log.i(TAG, "Success : " + result);
+            }
+
+            @Override
+            public void failure(Exception ex) {
+                //finish();
+            }
+        }, Utils.getToken(ContactViewActivity.this), BASE_URL + GET_CONTACT + b.getInt("id")).execute();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
