dd8ac56acb5c5a54b8867e3ac38845c7fc825d14
galedric <g@ledric.me>
2016-05-31 11:28:41 +0200

    Push comments

--- server/app/services/PushService.scala
+++ server/app/services/PushService.scala
@@ -1,20 +1,38 @@
 package services
 
 import com.google.inject.Singleton
-import play.api.libs.json.{JsArray, JsValue, Json}
+import play.api.libs.json.{JsArray, JsObject, JsValue, Json}
 import scala.collection.mutable
 import scala.concurrent.{Future, Promise}
 
+/**
+  * Handle HTTP long-polling notifications
+  */
 @Singleton
 class PushService {
+	/** Maximum size of the replay vectors */
 	private final val REPLAY_SIZE = 10
 
-	private val open = mutable.Map.empty[Int, List[Promise[JsValue]]].withDefaultValue(List.empty)
+	/** Pending promises */
+	private val open = mutable.Map.empty[Int, List[Promise[JsObject]]].withDefaultValue(List.empty)
+
+	/** Replay buffers */
 	private val replay = mutable.Map.empty[Int, Vector[(Int, JsValue)]].withDefaultValue(Vector.empty)
 
+	/** The next message ID */
 	private var next_id = 1
 
-	def register(user: Int, from_id: Option[Int]): Future[JsValue] = this.synchronized {
+	/**
+	  * Awaits for the next notification event.
+	  *
+	  * If the ?from parameter was given, this method will attempt to send
+	  * any missed event immediately.
+	  *
+	  * @param user    the user awaiting notification
+	  * @param from_id the last received notification id
+	  * @return a future that will be completed when an event is available
+	  */
+	def register(user: Int, from_id: Option[Int]): Future[JsObject] = this.synchronized {
 		val missed =
 			from_id.map { from =>
 				replay(user).collect { case (id, value) if id <= from => value }
@@ -26,13 +44,18 @@ class PushService {
 				"next" -> next_id
 			))
 		} else {
-			val promise: Promise[JsValue] = Promise()
+			val promise: Promise[JsObject] = Promise()
 			open(user) = promise :: open(user)
 			promise.future
 		}
 	}
 
-	def send(user: Int, data: JsValue) = this.synchronized {
+	/**
+	  * Sends an event to a user.
+	  * @param user the receiver user
+	  * @param data the event data
+	  */
+	def send(user: Int, data: JsValue): Unit = this.synchronized {
 		val ru = replay(user) :+ (next_id, data)
 		replay(user) = if (ru.size > REPLAY_SIZE) ru.drop(ru.size - REPLAY_SIZE) else ru
 		next_id += 1
